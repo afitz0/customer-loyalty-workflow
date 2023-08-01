4f3ffe602dffd640bc598490e18754c85bbf5527
Andrew Fitz Gibbon <fitz@temporal.io>
2023-08-01 10:31:25 -0700

    Convert StatusTier and Customer to plain classes to reduce required Java version.

--- java/src/main/java/actorworkflow/Customer.java
+++ java/src/main/java/actorworkflow/Customer.java
@@ -5,16 +5,20 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 @JsonInclude(Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Customer(
-        String customerId,
-        String name,
-        int loyaltyPoints,
-        StatusTier status,
-        ArrayList<Customer> guests
-) {
+public final class Customer {
+    private String customerId;
+    private String name;
+    private int loyaltyPoints;
+    private StatusTier status;
+    private ArrayList<Customer> guests;
+
+    public Customer() {
+    }
+
     public Customer(String customerId) {
         this(customerId,
                 "",
@@ -23,33 +27,76 @@ public record Customer(
                 new ArrayList<>());
     }
 
-    public Customer {
+    public Customer(String customerId, String name, int loyaltyPoints, StatusTier status, ArrayList<Customer> guests) {
         if (status == null) {
             status = StatusTier.STATUS_TIERS.get(0);
         }
         if (guests == null) {
             guests = new ArrayList<>();
         }
+        this.customerId = customerId;
+        this.name = name;
+        this.loyaltyPoints = loyaltyPoints;
+        this.status = status;
+        this.guests = guests;
     }
 
     public Customer withPoints(int points) {
-        return new Customer(
-                customerId,
-                name,
-                points,
-                status,
-                guests
-        );
+        this.loyaltyPoints = points;
+        return this;
     }
 
     public Customer withStatus(StatusTier status) {
-        return new Customer(
-                customerId,
-                name,
-                loyaltyPoints,
-                status,
-                guests
-        );
+        this.status = status;
+        return this;
+    }
+
+
+    public String customerId() {
+        return customerId;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public int loyaltyPoints() {
+        return loyaltyPoints;
+    }
+
+    public StatusTier status() {
+        return status;
+    }
+
+    public ArrayList<Customer> guests() {
+        return guests;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Customer) obj;
+        return Objects.equals(this.customerId, that.customerId) &&
+                Objects.equals(this.name, that.name) &&
+                this.loyaltyPoints == that.loyaltyPoints &&
+                Objects.equals(this.status, that.status) &&
+                Objects.equals(this.guests, that.guests);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customerId, name, loyaltyPoints, status, guests);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer[" +
+                "customerId=" + customerId + ", " +
+                "name=" + name + ", " +
+                "loyaltyPoints=" + loyaltyPoints + ", " +
+                "status=" + status + ", " +
+                "guests=" + guests + ']';
     }
 
     public static boolean canAddGuest(Customer customer) {
--- java/src/main/java/actorworkflow/StatusTier.java
+++ java/src/main/java/actorworkflow/StatusTier.java
@@ -3,15 +3,39 @@ package actorworkflow;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
-public record StatusTier(String name, int minimumPoints, int guestsAllowed) {
+public final class StatusTier {
     public static final List<StatusTier> STATUS_TIERS = new ArrayList<>(Arrays.asList(
-            new StatusTier("Member", 0, 0),
-            new StatusTier("Bronze", 500, 1),
-            new StatusTier("Silver", 1_000, 2),
-            new StatusTier("Gold", 2_000, 5),
-            new StatusTier("Platinum", 5_000, 10)
+            new StatusTier("Member", 0, 0, 0),
+            new StatusTier("Bronze", 500, 1, 1),
+            new StatusTier("Silver", 1_000, 2, 2),
+            new StatusTier("Gold", 2_000, 5, 3),
+            new StatusTier("Platinum", 5_000, 10, 4)
     ));
+
+    private final String name;
+    private final int minimumPoints;
+    private final int guestsAllowed;
+    private final int level;
+
+    public StatusTier() {
+        this(STATUS_TIERS.get(0));
+    }
+
+    public StatusTier(StatusTier tier) {
+        this.name = tier.name();
+        this.minimumPoints = tier.minimumPoints();
+        this.guestsAllowed = tier.guestsAllowed();
+        this.level = tier.level();
+    }
+
+    public StatusTier(String name, int minimumPoints, int guestsAllowed, int level) {
+        this.name = name;
+        this.minimumPoints = minimumPoints;
+        this.guestsAllowed = guestsAllowed;
+        this.level = level;
+    }
 
     public static StatusTier next(StatusTier tier) {
         int nextIndex = STATUS_TIERS.indexOf(tier) + 1;
@@ -24,12 +48,51 @@ public record StatusTier(String name, int minimumPoints, int guestsAllowed) {
     }
 
     public static StatusTier getMaxTier(int points) {
-        for (int i = STATUS_TIERS.size() - 1; i >=0; i--) {
+        for (int i = STATUS_TIERS.size() - 1; i >= 0; i--) {
             StatusTier tier = STATUS_TIERS.get(i);
             if (points >= tier.minimumPoints()) {
                 return tier;
             }
         }
         return STATUS_TIERS.get(0);
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public int minimumPoints() {
+        return minimumPoints;
+    }
+
+    public int guestsAllowed() {
+        return guestsAllowed;
+    }
+
+    public int level() {
+        return level;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StatusTier that = (StatusTier) o;
+        return minimumPoints == that.minimumPoints && guestsAllowed == that.guestsAllowed && level == that.level && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, minimumPoints, guestsAllowed, level);
+    }
+
+    @Override
+    public String toString() {
+        return "StatusTier{" +
+                "name='" + name + '\'' +
+                ", minimumPoints=" + minimumPoints +
+                ", guestsAllowed=" + guestsAllowed +
+                ", level=" + level +
+                '}';
     }
 }
